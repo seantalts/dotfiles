066d44dbc7acbc448a3827b950c6b2aaf13c666b
Sean Talts <sean.talts@gmail.com>
2020-08-15 19:30:05 -0400

    xmonad

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -1,6 +1,21 @@
 import XMonad
+import XMonad.Config.Mate (mateConfig)
+import XMonad.Prompt (defaultXPConfig)
+import XMonad.Prompt.Shell (shellPrompt)
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Hooks.ManageDocks (avoidStruts, manageDocks)
 
-main = xmonad defaultConfig
+customBindings :: [(String, X ())]
+customBindings = [ ("M-p", shellPrompt defaultXPConfig)
+                 , ("M-S-p e", spawn "emacsclient -a '' -c")
+                 ]
+
+main :: IO ()
+main = do
+  (xmonad . (`additionalKeysP` customBindings)) mateConfig
     { modMask = mod4Mask -- user Super instead of Alt
-    , terminal = "urxvt"
+    , terminal = "mate-terminal --hide-menubar"
+    , layoutHook = avoidStruts $ layoutHook def
+    , manageHook = manageHook def <+> manageDocks
+    , startupHook = spawn "xmobar"
     }
