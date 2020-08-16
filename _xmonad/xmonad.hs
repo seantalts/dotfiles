528d8241b09e40025b979342500b2dfedafd1800
Sean Talts <sean.talts@gmail.com>
2020-08-15 20:47:45 -0400

    xmonad fixes

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -4,10 +4,13 @@ import XMonad.Prompt (defaultXPConfig)
 import XMonad.Prompt.Shell (shellPrompt)
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Hooks.ManageDocks (avoidStruts, manageDocks)
+import XMonad.Layout.ThreeColumns (ThreeCol (ThreeCol, ThreeColMid))
 
 customBindings :: [(String, X ())]
 customBindings = [ ("M-p", shellPrompt defaultXPConfig)
                  , ("M-S-p e", spawn "emacsclient -a '' -c")
+                 , ("M-S-p f", spawn "firefox")
+                 , ("M-S-p c", spawn "chromium")
                  ]
 
 main :: IO ()
@@ -15,7 +18,7 @@ main = do
   (xmonad . (`additionalKeysP` customBindings)) mateConfig
     { modMask = mod4Mask -- user Super instead of Alt
     , terminal = "mate-terminal --hide-menubar"
-    , layoutHook = avoidStruts $ layoutHook def
+    , layoutHook = avoidStruts (ThreeCol 1 (3/100) (1/2) ||| ThreeColMid 1 (3/100) (1/2) ||| ThreeCol 1 (3/100) (1/2))
     , manageHook = manageHook def <+> manageDocks
     , startupHook = spawn "xmobar"
     }
